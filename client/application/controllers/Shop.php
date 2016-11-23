3968402f33652d87210bcc2a57b586496701fc32
Hoa Quan Van <hong7kong90@gmail.com>
2016-11-23 17:03:03 +0700

    message

--- client/application/controllers/Shop.php
+++ client/application/controllers/Shop.php
@@ -16,15 +16,12 @@ class Shop extends CI_Controller {
 	
 
 	// Show all product in one Category
-<<<<<<< HEAD
+ 
 	public function view($id){
 		if(isset($id)){$shop_id = $id;}
-=======
-	public function detail($shop_id){
->>>>>>> 896f56a546c6b7fc769481e31766e04149140e79
 		$param= array('sid'=> $shop_id);
 		$list_product = $this->shop_model->getAllProduct($param);
-		print_r($list_product);
+		
 		$data['list_product'] = $list_product;
 		$data['shop_detail'] = $this->shop_model->findShop(array('id'=>$shop_id));
 		$data['template'] = 'shop/view';
